0830f8cbbb2daed880850558664e5b8bc219ff2f
Liaxibll <neitroneitro3@gmail.com>
2023-02-15 18:40:57 +0100

    add hi

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 print("Hello world!")
 print("Hello-hello")
 print("gfdgfg")
+<<<<<<< Updated upstream
 print("Hello friends!")
 print("Це рядок Валерії")
+=======
+print("Hello friends!")
+>>>>>>> Stashed changes
