18c17ec6d4a9ea3d16857230f685b7837abe9522
ArtX-code <poop402r@gmail.com>
2023-02-19 20:50:09 +0200

    Зробленi записи даних до зміних та наслідування до інших питань

--- bot.py
+++ bot.py
@@ -6,44 +6,53 @@ bot = telebot.TeleBot("5662928795:AAHhdf4WxBx_CGY1x5CU85Y5qR06Y3UgcFQ")
 
 name = ''
 surname = ''
-phone_number = ''
-email = ''
-education = ''
-skills = ''
-lang = ''
-lang_level = ''
-recommendations = ''
-location = ''
-work_experience = ''
+phone_number = ''           #Рома
+email = ''                  #Рома
+education = ''              #Діма
+skills = ''                 #Діма
+lang = ''                   #Діма
+lang_level = ''             #Назар
+location = ''               #Назар
+work_experience = ''        #Назар
+
+
+def but_create():
+    reply_markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    but1 = KeyboardButton('📄Створити резюме📄')
+    reply_markup.add(but1)
+    return reply_markup
 
 
 @bot.message_handler(commands=['start'])                            #можна додати команду, щоб редагувати вже записані відповіді(або кейборд кнопкою)
 def button_message(message):
-    reply_markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    but1 = KeyboardButton('📄Створити резюме📄')
-    reply_markup.add(but1)
     bot.send_message(message.chat.id, 'Привіт,{}!\n '
-                                      'Це бот для створення резюме, думаю тобі сподобається'.format(message.from_user.first_name), reply_markup=reply_markup)
+                                      'Це бот для створення резюме, думаю тобі сподобається'.format(message.from_user.first_name), reply_markup=but_create())
+    print(message)
 
 
 @bot.message_handler(content_types=['text'])
 def message_reply(message):
+    global name
     if message.text == '📄Створити резюме📄':
-        reply_markup1 = ReplyKeyboardMarkup(resize_keyboard=True)               # Пропоную кнопки написати у окремиих функціях
-        but2 = KeyboardButton("Запам'ятати відповідь")                          # Треба записувати дані користувача в зміні
-        reply_markup1.add(but2)
-        bot.send_message(message.chat.id, 'Напишіть ваше ім’я', reply_markup=reply_markup1)
-    elif message.text == "Запам'ятати відповідь":                               # Після відхилення код буде йти по колу
-        reply_markup2 = ReplyKeyboardMarkup(resize_keyboard=True)
-        but3 = KeyboardButton("Так")
-        but4 = KeyboardButton("Ні")
-        reply_markup2.add(but3, but4)
-        bot.send_message(message.chat.id, 'Чи хочете ви відредагувати відповідь на запитання?', reply_markup=reply_markup2)
-    elif message.text == "Ні":
         reply_markup1 = ReplyKeyboardMarkup(resize_keyboard=True)
-        but2 = KeyboardButton("Запам'ятати відповідь")
-        reply_markup1.add(but2)
-        bot.send_message(message.chat.id, 'Напишіть ваше прізвище', reply_markup=reply_markup1)
+        msg = bot.send_message(message.chat.id, 'Напишіть ваше ім’я', reply_markup=reply_markup1)
+        bot.register_next_step_handler(msg, get_user_name)
+
+
+def get_user_name(message):
+    global name
+    if message.text:
+        name = message.text
+    msg = bot.send_message(message.chat.id, 'Напишіть ваше прізвище')
+    bot.register_next_step_handler(msg, get_surname)
+    print('name = ', name)
+
+
+def get_surname(message):
+    global surname
+    if message.text:
+        surname = message.text
+        print('surname=', surname)
 
 
 bot.polling(none_stop=True)
