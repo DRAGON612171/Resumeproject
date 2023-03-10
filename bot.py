f428fe26e77a35e6364a092034bacce1bb7b2fb9
ArtX-code <poop402r@gmail.com>
2023-03-10 23:21:53 +0200

    1

--- bot.py
+++ bot.py
@@ -1,3 +1,38 @@
+def get_skills(message):
+    global skills
+    if message.text:
+        skills = message.text
+    elif message.text == '-':
+        pass
+    msg = bot.send_message(message.chat.id, 'Напишіть якими мовами ви володієте')
+    bot.register_next_step_handler(msg, get_lang)
+    print('skills = ', skills)
+
+
+def get_lang(message):
+    global lang
+    if message.text:
+        lang = message.text
+    elif message.text == '-':
+        pass
+
+    msg = bot.send_message(message.chat.id, 'Напишіть про рівень знання цих мов')         #замість другого напиши, що ти хочеш питати наступне(lang_level)
+    bot.register_next_step_handler(msg, get_lang_level)                                #замість другого напиши наступну функцію
+    print('lang = ', lang)
+
+
+def get_lang_level(message):
+    global lang_level
+    if message.text:
+        lang_level = message.text
+    elif message.text == '-':
+        pass
+
+    msg = bot.send_message(message.chat.id, 'Напишіть де ви живете ')
+    bot.register_next_step_handler(msg, get_location)
+    print('lang_level = ', lang_level)
+
+import time
 import telebot
 from telebot.types import InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup
 import random
